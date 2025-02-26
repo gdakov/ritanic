c0a84eca32bfc3d4fba18b6a6c3895b0aea2c717
G Dakov <goran.dakov@gmail.com>
2025-02-26 18:41:07 +0100

    Update llvm-butcher.cpp

--- src/llvm-butcher.cpp
+++ src/llvm-butcher.cpp
@@ -1,1 +1,8 @@
+struct insn { int opA; int opB; int opT;
+int oper; int widthi; int labl1; int labl2; };
 
+std::vector<insn> read_block( FILE * f) {
+    if (5==fscanf("[ \t]*%%%s[ \t]*=[ \t]i%i[ \t]*%s[ \t]* %%%s[ \t]* %%%s\n",rt,name,isz,ra,rb)){
+      ;
+    }
+}
