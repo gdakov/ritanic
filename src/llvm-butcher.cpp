9d2f3af0915b504ccb97efe5bb988c59db368486
G Dakov <goran.dakov@gmail.com>
2025-02-26 18:59:37 +0100

    Update llvm-butcher.cpp

--- src/llvm-butcher.cpp
+++ src/llvm-butcher.cpp
@@ -3,6 +3,19 @@ int oper; int widthi; int labl1; int labl2; };
 
 std::vector<insn> read_block( FILE * f) {
     if (5==fscanf("[ \t]*%%%s[ \t]*=[ \t]i%i[ \t]*%s[ \t]* %%%s[ \t]* %%%s\n",rt,name,isz,ra,rb)){
-      ;
+      if (atomtbl.count(rt)) {
+      };
+      atomtbl[rt]=idxautoinc();
+      if (!atomtbl.count(rA) || !atomtbl.count(rB)) {
+      }
+      if (!infoptab.count(name)) {
+      }
+      insn i;
+      i.rT=rt;
+      I.rA=rA;
+      I.rB=rB;
+      I.widthi=isz;
+      I.oper=infoptab[name];
+      ret.push_back(i);
     }
 }
