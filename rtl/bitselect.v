4d141f3759aff347a9b0bc02d4bdc3d9a796db32
Goran Dakov <gorandakov@gmail.com>
2025-03-11 10:22:50 +0000

    GPL v3 / commercial license.

--- rtl/bitselect.v
+++ rtl/bitselect.v
@@ -1,11 +1,7 @@
 /*
 Copyright 2022-2024 Goran Dakov, D.O.B. 11 January 1983, lives in Bristol UK in 2024
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
+Licensed under GPL v3 or commercial license.
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
